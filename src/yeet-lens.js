7d00ccfad78881155a614e0de837f429cf08cfad
bob <bcoop713@gmail.com>
2019-07-23 12:56:37 -0400

    wip compose

--- src/yeet-lens.js
+++ src/yeet-lens.js
@@ -1,6 +1,6 @@
 // ROOT
 export const rootGetter = state => state;
-export const rootSetter = (arg, state) => {
+export const rootSetter = arg => state => {
   if (typeof arg === "function") {
     return arg(state);
   }
@@ -9,20 +9,25 @@ export const rootSetter = (arg, state) => {
 export const rootLens = [rootGetter, rootSetter];
 
 // ARRAY
-export const arrayGetter = (i, state) => rootGetter(state[i]);
-export const arraySetter = (i, arg, state) =>
+export const arrayGetter = i => state => rootGetter(state[i]);
+export const arraySetter = (i, arg) => state =>
   state.map((item, index) => {
     if (index === i) {
-      return rootSetter(arg, item);
+      return rootSetter(arg)(item);
     }
     return item;
   });
 export const arrayLens = [arrayGetter, arraySetter];
 
 // OBJECT
-export const keyGetter = (key, state) => rootGetter(state[key]);
-export const keySetter = (key, arg, state) => ({
+export const keyGetter = key => state => rootGetter(state[key]);
+export const keySetter = (key, arg) => state => ({
   ...state,
-  [key]: rootSetter(arg, state[key])
+  [key]: rootSetter(arg)(state[key])
 });
 export const keyLens = [keyGetter, keySetter];
+
+// COMPLEX
+export const composeGetter = (...args) => state => {
+  args.reverse();
+};
--- src/yeet-lens.test.js
+++ src/yeet-lens.test.js
@@ -6,36 +6,36 @@ describe("root", () => {
     expect(L.rootGetter(atomState)).toBe("cat");
   });
   test("root setter with value", () => {
-    expect(L.rootSetter("dog", atomState)).toBe("dog");
+    expect(L.rootSetter("dog")(atomState)).toBe("dog");
   });
   test("root setter with function", () => {
-    expect(L.rootSetter(state => state + "s", atomState)).toBe("cats");
+    expect(L.rootSetter(state => state + "s")(atomState)).toBe("cats");
   });
 });
 
 describe("array", () => {
   const atomState = [1, 2, 3];
   test("array getter", () => {
-    expect(L.arrayGetter(1, atomState)).toBe(2);
+    expect(L.arrayGetter(1)(atomState)).toBe(2);
   });
   test("array setter with value", () => {
-    expect(L.arraySetter(1, "dog", atomState)).toEqual([1, "dog", 3]);
+    expect(L.arraySetter(1, "dog")(atomState)).toEqual([1, "dog", 3]);
   });
   test("root setter with function", () => {
-    expect(L.arraySetter(1, state => state + 1, atomState)).toEqual([1, 3, 3]);
+    expect(L.arraySetter(1, state => state + 1)(atomState)).toEqual([1, 3, 3]);
   });
 });
 
 describe("object", () => {
   const atomState = { a: 1 };
   test("key getter", () => {
-    expect(L.keyGetter("a", atomState)).toBe(1);
+    expect(L.keyGetter("a")(atomState)).toBe(1);
   });
   test("key setter with value", () => {
-    expect(L.keySetter("a", 3, atomState)).toEqual({ a: 3 });
+    expect(L.keySetter("a", 3)(atomState)).toEqual({ a: 3 });
   });
   test("key setter with function", () => {
-    expect(L.keySetter("a", state => state + 1, atomState)).toEqual({ a: 2 });
+    expect(L.keySetter("a", state => state + 1)(atomState)).toEqual({ a: 2 });
   });
 });
 
